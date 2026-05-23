08882c36d8b9b7c77f3fcb9be63352a43b3fd07c
umang00arora <umangarora810@gmail.com>
2026-05-23 21:01:33 +0530

    Added instructions

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ function App() {
         Umang Gestures
       </h1>
 
+      <p className="instructions">
+  1️⃣ 😎 Glasses &nbsp; | &nbsp;
+  2️⃣ 👑 Crown &nbsp; | &nbsp;
+  3️⃣ 🥸 Mustache &nbsp; | &nbsp;
+  4️⃣ 🤡 Clown &nbsp; | &nbsp;
+  5️⃣ 👀 Anime Eyes
+</p>
+
       <HandTracker />
 
     </div>
